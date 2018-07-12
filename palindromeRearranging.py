8d0d8331964d8b9cbacae9be0b94f3b47116d560
Dmytriy Myk <porosya@gmail.com>
2018-07-13 00:10:42 +0300

    done

--- palindromeRearranging.py
+++ palindromeRearranging.py
@@ -1,2 +1,14 @@
 def palindromeRearranging(inputString):
-    return 
+    result = [True if not (inputString.count(x) % 2) else False for x in set(inputString)]
+
+    return True if result.count(False) < 2 else False
+
+
+def palindromeRearranging_smart(inputString):
+
+    return sum([inputString.count(i) % 2 for i in set(inputString)]) <= 1
+
+
+
+inputString = "abbcabb"
+print(palindromeRearranging(inputString))
