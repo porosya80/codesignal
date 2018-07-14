1395e5315aa45fd5df0ed0096c86e630cc4c31d8
Dmytriy Myk <porosya@gmail.com>
2018-07-14 20:05:55 +0300

    done

--- minesweeper.py
+++ minesweeper.py
@@ -1,23 +1,16 @@
 def minesweeper(matrix):
     offset = (0, 1, -1)
-    # indices = [(i, j) for i in offset for j in offset]
     bombs = [(x, y) for x, row in enumerate(matrix) for y, item in enumerate(row) if item]
-    matrix = [["b" if x else 0 for x in row] for row in matrix]
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            if matrix[row][column] == "b":
-                matrix[row][column]
-                indices = [(row + x, column + y) for x in offset if 0 <= row + x <= len(matrix)-1 for y in offset if 0 <= column + y <= len(matrix[0])-1]
-                # print(row, column)
-                # print(indices[1:])
+    matrix = [[0 for x in row] for row in matrix]
+    for bomb in bombs:
+        row, column = bomb
+        indices = [(row + x, column + y) for x in offset if 0 <= row + x <= len(matrix)-1 for y in offset if 0 <= column + y <= len(matrix[0])-1]
+        for pos in indices[1:]:
+            x, y = pos
+            matrix[x][y] += 1
+    return matrix
 
-                for pos in indices[1:]:
-                    # print(pos)
-                    x, y = pos
-                    if matrix[x][y] != "b":
-                        matrix[x][y] += 1
 
-    return [[1 if x == "b" else x for x in row] for row in matrix]
 
 
 matrix = [[True, False, False, True],
