5b9001618fa5b59fbfbd5ac5fd6f86cbb4a1d247
Dmytriy Myk <porosya@gmail.com>
2018-07-25 19:45:21 +0300

    sudoku finished

--- killKthBit.py
+++
@@ -1,8 +0,0 @@
-def killKthBit(n, k):
-    return ...
-
-
-n = 37
-k = 3
-print(killKthBit(n, k))
-#  = 33
--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,24 @@
+def sudoku(grid):
+    moves = [(x, y) for x in [1, 0, -1] for y in [1, 0, -1]]
+    etal = set(range(1, 10))
+    # check = []
+    for row in grid:
+        if set(row) != etal:
+            return False
+    for row in zip(*list(grid)):
+        if set(row) != etal:
+            return False
+    for row in range(1, 9, 3):
+        for col in range(1, 9, 3):
+            check = []
+            for x, y in moves:
+                check.append(grid[row+x][col+y])
+                # print(grid[row][col])
+
+            if set(check) != etal:
+
+                return False
+    return True
 
 
 grid = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
