45e915e54840656708da76241160579981b793b0
Dmytriy Myk <porosya@gmail.com>
2018-12-04 03:52:47 +0200

    solved arefollowingpatterns

--- areFollowingPatterns.py
+++ areFollowingPatterns.py
@@ -1,11 +1,26 @@
 def areFollowingPatterns(strings, patterns):
+    result = True
     string_to_pattern = {}
     pattern_to_string = {}
+    for string, pattern in zip(strings, patterns):
+        # print(string, pattern)
+        if string not in string_to_pattern.keys():
+            string_to_pattern[string] = pattern
+        if pattern not in pattern_to_string.keys():
+            pattern_to_string[pattern] = string
+
+    for string, pattern in zip(strings, patterns):
+        # print(string_to_pattern[string], pattern_to_string[pattern])
+        if string_to_pattern[string] != pattern or pattern_to_string[pattern] != string:
+            result = False
+            break
+
+    return result
 
 
 strings = ["cat",
-           "dog",
-           "dog"]
+           "dod",
+           "dod"]
 patterns = ["a",
             "b",
             "b"]
