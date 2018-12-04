70a13571353c9aa03e9d7acf7e08cce35c238153
Dmytriy Myk <porosya@gmail.com>
2018-12-04 03:16:54 +0200

    added arefollowingpatterns

---
+++ areFollowingPatterns.py
@@ -0,0 +1,14 @@
+def areFollowingPatterns(strings, patterns):
+    string_to_pattern = {}
+    pattern_to_string = {}
+
+
+strings = ["cat",
+           "dog",
+           "dog"]
+patterns = ["a",
+            "b",
+            "b"]
+areFollowingPatterns(strings, patterns)
+# Expected Output:
+# true
